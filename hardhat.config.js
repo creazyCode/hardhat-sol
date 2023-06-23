40ec1caf0799812a7a2ccbcc7753f4f04d4e7faa
achilles <xavier19921229@gmail.com>
2023-06-24 01:49:03 +0800

    feat<all>: 添加goerli网络和账户配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,5 +8,22 @@
 //这里需要引入hardhat-toolbox包，否则会识别不了ethers这个关键字
 require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://rpc.ankr.com/eth_goerli",
+      accounts: ["d19b310f285a525362552e15aea8d01ad78646ee05b7f92bb90dd77c9138b49a", "d19b310f285a525362552e15aea8d01ad78646ee05b7f92bb90dd77c9138b49a"]
+    }
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
 };
