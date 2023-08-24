cbf061448c70f44d7c5554d6da367a857367bc89
achilles <xavier19921229@gmail.com>
2023-08-24 22:18:38 +0800

    feat<base>: 修改erc20符号

--- coinbase/contracts/Erc20.sol
+++ coinbase/contracts/Erc20.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract PHToken is ERC20, ERC20Permit {
-    constructor() ERC20("PHToken", "PH") ERC20Permit("PHToken") {
+    constructor() ERC20("Sui Metaverse", "SM") ERC20Permit("SM") {
      _mint(msg.sender, 10 * (10 ** decimals()));
     }
 
--- coinbase/scripts/deployerc20.ts
+++ coinbase/scripts/deployerc20.ts
@@ -1,14 +1,20 @@
+/*
+ * @@Description: TODO
+ * @@Author: tx
+ * @@Date: Do not edit
+ */
 import { ethers } from "hardhat";
 
 async function main() {
   const ERC20 = await ethers.getContractFactory("PHToken");
   const ERC20_TX = await ERC20.deploy();
-  const mintToken = await ERC20_TX.mint("0xDF010C965A70b76E5a57CB2E3A786831174c99c3", 1000000000);
-  
+    
   const ContractAddress = await ERC20_TX.getAddress();
 
+  // const mintToken = await ERC20_TX.mint("0xDF010C965A70b76E5a57CB2E3A786831174c99c3", 1000000000);
+
   console.log(
-    `Counter contract deployed to https://explorer.public.zkevm-test.net/address/${ContractAddress}`
+    `Counter contract deployed to https://basescan.org/address/${ContractAddress}`
   );
 
   //mint token : 合约里在创建合约时已经初始化mint到deployer地址里了
